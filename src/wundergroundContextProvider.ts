4f64befa5cf0c75e317a79bed4fc446bf5dcc511
Fabian Bäumer <fabianbaeumer@gmail.com>
2019-09-23 12:02:24 +0200

    Check entity type

--- src/wundergroundContextProvider.ts
+++ src/wundergroundContextProvider.ts
@@ -12,56 +12,64 @@ async function handleContextRequest(req : Request, res : Response) {
     let response = new ProviderResponse();
     for(let i = 0; i < req.body.entities.length; i++) {
         let contextResponse = new ContextResponse();
-        // Mirror id, type and isPattern from entity
-        contextResponse.contextElement.id = req.body.entities[i].id;
-        contextResponse.contextElement.isPattern = req.body.entities[i].isPattern;
-        contextResponse.contextElement.type = req.body.entities[i].type;
-        // By default a context request will be successful
-        contextResponse.statusCode = { code: "200", reasonPhrase: "OK" };
-
-        // Expected id format: urn:ngsi-ld:WeatherObserved:<Station ID>
-        let stationId = req.body.entities[i].id.split(':')[3];
         try {
-            let observation = await getCurrentConditions(stationId);
-            let normalizedWeatherObserved = observation.toWeatherObserved().normalize();
-            for(let j = 0; j < req.body.attributes.length; j++) {
-                if(normalizedWeatherObserved[req.body.attributes[j]] === undefined) {
-                    // Attribute not defined
-                    debug("Attribute '%s' is not defined on given instance of WeatherObserved (index %d)", req.body.attributes[j], i);
-                    continue;
+            // Mirror id, type and isPattern from entity
+            contextResponse.contextElement.id = req.body.entities[i].id;
+            contextResponse.contextElement.isPattern = req.body.entities[i].isPattern;
+            contextResponse.contextElement.type = req.body.entities[i].type;
+            if(contextResponse.contextElement.type !== "WeatherObserved") {
+                // This context provider only supports WeatherObserved type
+                debug("Unable to serve context element of type '%s'. Only type 'WeatherObserved' is supported", contextResponse.contextElement.type);
+                contextResponse.statusCode = { code: "400", reasonPhrase: "Invalid context element type caught, only 'WeatherObserved' is supported by this provider" };
+                continue;
+            }
+            // By default a context request will be successful
+            contextResponse.statusCode = { code: "200", reasonPhrase: "OK" };
+
+            // Expected id format: urn:ngsi-ld:WeatherObserved:<Station ID>
+            let stationId = req.body.entities[i].id.split(':')[3];
+            try {
+                let observation = await getCurrentConditions(stationId);
+                let normalizedWeatherObserved = observation.toWeatherObserved().normalize();
+                for(let j = 0; j < req.body.attributes.length; j++) {
+                    if(normalizedWeatherObserved[req.body.attributes[j]] === undefined) {
+                        // Attribute not defined
+                        debug("Attribute '%s' is not defined on given instance of WeatherObserved (index %d)", req.body.attributes[j], i);
+                        continue;
+                    }
+                    contextResponse.contextElement.attributes.push({
+                        name: req.body.attributes[j],
+                        ...normalizedWeatherObserved[req.body.attributes[j]]
+                    });
                 }
-                contextResponse.contextElement.attributes.push({
-                    name: req.body.attributes[j],
-                    ...normalizedWeatherObserved[req.body.attributes[j]]
-                });
             }
-        }
-        catch(e) {
-            if(e instanceof Error) {
-                debug("Encountered common error while processing context request for station id '%s'", stationId)
-                // Common error (Network, parsing, or something else)
-                contextResponse.statusCode = {
-                    code: "503", // Service Unavailable
-                    reasonPhrase: e.message
+            catch(e) {
+                if(e instanceof Error) {
+                    debug("Encountered common error while processing context request for station id '%s'", stationId)
+                    // Common error (Network, parsing, or something else)
+                    contextResponse.statusCode = {
+                        code: "503", // Service Unavailable
+                        reasonPhrase: e.message
+                    }
                 }
+                else if(e.msg !== undefined) {
+                    debug("Encountered API error while processing context request for station id '%s'", stationId)
+                    // API error
+                    contextResponse.statusCode = {
+                        code: e.code.toString(),
+                        reasonPhrase: e.msg
+                    };
+                }
+                else {
+                    debug("Encountered unknown error while processing context request for station id '%s'", stationId)
+                    // Unknown error
+                    contextResponse.statusCode = {
+                        code: "500",
+                        reasonPhrase: "Encountered unknown error while processing context element"
+                    };
+                }
+                debug("%O", e)
             }
-            else if(e.msg !== undefined) {
-                debug("Encountered API error while processing context request for station id '%s'", stationId)
-                // API error
-                contextResponse.statusCode = {
-                    code: e.code.toString(),
-                    reasonPhrase: e.msg
-                };
-            }
-            else {
-                debug("Encountered unknown error while processing context request for station id '%s'", stationId)
-                // Unknown error
-                contextResponse.statusCode = {
-                    code: "500",
-                    reasonPhrase: "Encountered unknown error while processing context element"
-                };
-            }
-            debug("%O", e)
         }
         finally {
             response.contextResponses.push(contextResponse);
