18dc9bd358497440acc33e65467052df946445ac
Fabian Bäumer <fabian.baeumer@rub.de>
2020-01-12 20:28:15 +0100

    Create suitable ESlint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project:  [
+            './tsconfig.json'
+        ]
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    rules: {
+    }
+};
