85f1aec222751ded93bbeea1f2517b98e29c4974
Fabian Bäumer <fabian.baeumer@rub.de>
2020-01-15 17:16:54 +0100

    Make error responses NGSIv2 conforming

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -1,2 +1,7 @@
 export class ValueError extends Error { }
-export class WundergroundAPIError extends Error { }
+export class WundergroundAPIError extends Error {
+    constructor(
+        public statusCode: number = 0,
+        public message: string
+    ) { super(message); }
+}
