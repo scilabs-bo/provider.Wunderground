80ec5e055bc876866351a3d6099e05373cbaadc4
Fabian Bäumer <fabian.baeumer@rub.de>
2020-01-15 17:28:33 +0100

    Manually set prototype on custom errors See https://github.com/Microsoft/TypeScript/issues/13965

--- src/exceptions.ts
+++ src/exceptions.ts
@@ -1,7 +1,17 @@
-export class ValueError extends Error { }
+export class ValueError extends Error {
+    constructor(
+        public message: string
+    ) {
+        super(message);
+        Object.setPrototypeOf(this, ValueError.prototype);
+    }
+}
 export class WundergroundAPIError extends Error {
     constructor(
         public statusCode: number = 0,
         public message: string
-    ) { super(message); }
+    ) {
+        super(message);
+        Object.setPrototypeOf(this, WundergroundAPIError.prototype);
+    }
 }
