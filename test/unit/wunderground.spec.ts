4cf51e22e02d7303bedb264e72f106458e062ee7
Fabian Bäumer <fabian.baeumer@rub.de>
2020-01-15 17:34:11 +0100

    Make wunderground.spec.ts independed

--- test/unit/wunderground.spec.ts
+++ test/unit/wunderground.spec.ts
@@ -1,3 +1,7 @@
+// We need to set any 32 character hex string as api key to pass the verify call in config
+// This hex string is randomly generated and does not work on the wunderground api
+process.env['PROVIDER_WUNDERGROUND_API_KEY'] = '570335548314bdb25ed8e654bf9afc8c';
+
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import nock from 'nock';
